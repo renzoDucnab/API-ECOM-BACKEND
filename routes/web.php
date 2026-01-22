4b3f33d13b2359b308c9caf4b8711114cbd80cf6
renzoDucnab <bancudzo3@gmail.com>
2026-01-22 11:04:24 +0800

    Add health check

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'E-Commerce API',
+        'timestamp' => now()
+    ]);
+});
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
 });
